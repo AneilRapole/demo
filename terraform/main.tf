6e4d9dec5b1994923df9067e30b7b9f4222ea7d6
qwikfeb-coder <anielrapole@gmail.com>
2026-04-24 11:32:13 +0530

    resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
-terraform {
-  required_version = ">= 1.0"
+# 1. VPC Configuration
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
 }
-
-provider "null" {}
-# trigger updates
---
+++ terraform/provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+    project     = "rock-position-455809-m2"
+  region      = var.region
+}
