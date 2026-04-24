b791a4ffdaf7d11107cec14391ca251d8779706f
qwikfeb-coder <anielrapole@gmail.com>
2026-04-24 13:34:37 +0530

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,20 @@
 # 1. VPC Configuration
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+}
+
 resource "google_compute_network" "vpc_network" {
   name                    = "vpc-network"
   auto_create_subnetworks = false
 }
+
